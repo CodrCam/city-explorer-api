d8596003f76371aca516448c3349877a7485bc77
Cameron Griffin <cambackcountry@gmail.com>
2023-03-21 21:56:56 -0700

    card 2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
-
+//IMPORTS
+const axios = require('axios');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -7,29 +8,60 @@ const cors = require('cors');
 const app = express();
 let data = require('./data/weather.json');
 
-//middlewware
+// Middleware
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
 
-app.on('listening', () => {
-  console.log(`We are running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`We are running on port ${PORT}`));
 
-app.listen(PORT);
-
-//end points
+// Endpoints
 
 app.get('/', (request, response) => {
   response.status(200).send('Welcome to my server!');
 });
 
-// CATCH ALL -- This should be added after all other routes
+class Forecast {
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
+}
+
+app.get('/weather', async (request, response, next) => {
+  try {
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    let searchQuery = request.query.searchQuery;
+
+    // Use the .find() method to determine which city the search query belongs to
+    let city = data.find(city => {
+      return city.lat === lat && city.lon === lon || city.city_name.toLowerCase() === searchQuery.toLowerCase();
+    });
+
+    if (!city) {
+      throw new Error('City not found. Please search for Seattle, Paris, or Amman.');
+    }
+
+    // Create an array of Forecast objects, one for each day in the city's weather data
+    const forecasts = city.data.map(day => {
+      return new Forecast(day.datetime.slice(0, 10), day.weather.description);
+    });
+
+    // Return the forecasts array to the client as a JSON object
+    response.status(200).json(forecasts);
+  } catch (error) {
+    console.log(error.message);
+    next(error.message);
+  }
+});
+
+// Catch-all -- This should be added after all other routes
 app.get('*', (request, response) => {
   response.status(404).send('Sorry, this route does not exist.');
 });
 
-// **** ERROR HANDLING - PLUG AND PLAY CODE FROM EXPRESS DOCS ****
+// Error handling middleware
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
 });
